14c1315217a70ebc905efe7d212ecd3c18243b60
daviddevops2000 <david.devops2000@gmail.com>
2020-05-26 02:09:36 -0300

    foi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,6 @@ export default function Home() {
 
   return (
   <>  
-    <Menu/>
-      <body style={{backgroundColor:"#ADD8E6"}}>
-      <h1 className="textHome" style={{textAlign:"center"}}> Seja Bem-Vindo ao Site Ao Espaço</h1>
-
-                     
-      </body>
     </>
     );
 }
